e583b127171e730c3b1458caf14da7ef7e05bb47
kavindu gayantha <96kavindugayantha@gmail.com>
2019-10-16 20:22:36 +0530

    first commit

---
+++ app/src/main/java/com/example/myapp2/third_signUp.kt
@@ -0,0 +1,12 @@
+package com.example.myapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class third_signUp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third_sign_up)
+    }
+}
